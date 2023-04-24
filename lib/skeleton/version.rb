a0aa40f5e6aa4e1208c58242092dd7eb54e029c9
dittmar <dittmar.krall@matique.de>
2023-04-24 09:56:33 +0200

    'working on v0.5.5'

--- lib/skeleton/version.rb
+++ lib/skeleton/version.rb
@@ -1,6 +1,7 @@
 # rubocop: disable all
 
 module Skeleton
-  VERSION = "0.5.4" # 2022-12-21
+  VERSION = "0.5.5" # 2023-04-24
+  #  VERSION = "0.5.4" # 2022-12-21
   # VERSION = "0.5.3" # 2021-06-21
 end
